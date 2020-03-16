d9808a460218a19986b8906f103c0ecbe5493478
skinnerrula <skinklass@mail.ru>
2020-03-16 13:23:53 +0300

    Initial Commit

---
+++ MyEateryRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyEateryRate
+//
+//  Created by Дмитрий Федоринов on 16.03.2020.
+//  Copyright © 2020 Дмитрий Федоринов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
