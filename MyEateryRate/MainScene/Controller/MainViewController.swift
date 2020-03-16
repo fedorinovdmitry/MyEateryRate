040234cdb8c3fabbcf4353d907d9507273b68ea8
skinnerrula <skinklass@mail.ru>
2020-03-16 22:32:17 +0300

    Настроил табл вью экрана списка ресторанов, добавил ячейки с именами и фотографии.

--- MyEateryRate/MainScene/Controller/MainViewController.swift
+++ MyEateryRate/MainScene/Controller/MainViewController.swift
@@ -14,6 +14,14 @@ class MainViewController: UIViewController {
     
     // MARK: - Constants
     
+    let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    
     // MARK: - Outlets
     
     // MARK: - Public Properties
@@ -34,17 +42,21 @@ class MainViewController: UIViewController {
 
 }
 
-// MARK: - TableView controller delegate
+// MARK: - TableView data source and delegate
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return restaurantNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CellID")
-        cell?.textLabel?.text = "Cell"
-        return cell!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath)
+        
+        let eateryName = restaurantNames[indexPath.row]
+        cell.textLabel?.text = eateryName
+        cell.imageView?.image = UIImage(named: eateryName)
+        
+        return cell
     }
 }
